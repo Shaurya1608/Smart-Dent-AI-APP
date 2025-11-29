5fbd656546cd4707e5c6d56cff0fe7359bc45984
Shaurya1608 <shauryak1608@example.com>
2025-11-29 14:14:02 +0530

    done syncing

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { SignedOut } from '@clerk/nextjs'
+import React from 'react'
+
+function DashboardPage() {
+  return (
+    <div>DashboardPage
+      
+    </div>
+
+  )
+}
+
+export default DashboardPage
